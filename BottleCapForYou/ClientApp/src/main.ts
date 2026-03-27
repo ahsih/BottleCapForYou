ba6607a5a1d0f30f96d491af26310a5cc656ad7d
Ahsih <ahsih1zhang@hotmail.com>
2026-03-27 21:55:56 +0000

    Push bottle cap for you website
    Push bottle cap for you website

---
+++ BottleCapForYou/ClientApp/src/main.ts
@@ -0,0 +1,15 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/home/home.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ])
+  ]
+}).catch((err) => console.error(err));
